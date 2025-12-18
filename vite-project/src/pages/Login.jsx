b4958f1f30bba38a5760a1ed4dff224325763318
sujonmajhi012-dot <sujonmajhi012@gmail.com>
2025-12-18 19:26:13 +0530

    Update Login.jsx

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
       return handleError('name,email and password are required !');
     }
     try{
-    const url="https://signup-login-api-three.vercel.app/auth/login";
+    const url="https://signup-login-api-seven.vercel.app/auth/login";
     const response = await fetch(url,
         {
           method:"POST",
